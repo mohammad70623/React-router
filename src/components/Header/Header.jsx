f4b56e281ca1cc3c72c36c99b2ec828fe151a7bf
Mohammad <mohammad70623@gmail.com>
2026-06-05 17:22:34 +0600

    add header nav links styling and root layout

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <h1>Header</h1>
+            <nav>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/mobiles">Mobiles</NavLink>
+                <NavLink to="/laptops">Laptops</NavLink>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,20 @@ import App from './App.jsx'
 
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import Mobiles from './components/Mobiles/Mobiles.jsx';
+import Laptops from './components/Laptops/Laptops.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    children: [ 
+      {index: true, Component: Home},
+      {path: 'mobiles', Component: Mobiles},
+      {path: 'laptops', Component: Laptops}
+    ]
   },
   {
     path: "about",
